d231778ba5d8976cd54c1139902253f99bbceda0
oleh <phd.chornyi@gmail.com>
2025-06-09 15:30:41 +0300

    1st: FadeIn and FadeOut

---
+++ lib/implicit_animations/1_fade_in.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+const owlUrl =
+    'https://raw.githubusercontent.com/flutter/website/main/src/content/assets/images/docs/owl.jpg';
+
+class FadeIn extends StatefulWidget {
+  const FadeIn({super.key});
+
+  @override
+  State<FadeIn> createState() => _FadeInState();
+}
+
+class _FadeInState extends State<FadeIn> {
+  double opacity = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Image.network(owlUrl),
+          TextButton(
+            child: const Text(
+              'Show Details',
+              style: TextStyle(color: Colors.blueAccent),
+            ),
+            onPressed:
+                () => setState(() {
+                  if (opacity == 1) {
+                    opacity = 0;
+                  } else {
+                    opacity = 1;
+                  }
+                }),
+          ),
+          const Column(
+            children: [
+              Text('Type: Owl'),
+              Text('Age: 39'),
+              Text('Employment: None'),
+            ],
+          ),
+          AnimatedOpacity(
+            duration: const Duration(seconds: 2),
+            opacity: opacity,
+            child: const Column(
+              children: [
+                Text('Type: Owl'),
+                Text('Age: 39'),
+                Text('Employment: None'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
